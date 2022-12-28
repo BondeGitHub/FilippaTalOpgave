2cd3155b753fe9057df2861f5c4b2f9903aa3ea0
Bonde Bonde <hotbonde@hotmail.com>
2022-12-28 14:33:07 +0100

    Added support for CancellationToken/CancellationTokenSource to be able to stop the program when result found on one task (preventing the other tasks to continue running).

--- FilippaTalOpgave/Program.cs
+++ FilippaTalOpgave/Program.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace FilippaTalOpgave
 {
-    class Program
+     class Program
     {
+        static readonly CancellationTokenSource cts = new CancellationTokenSource();
+
         static async Task Main(string[] args)
-        //JBD 06-09-2020 :  static async Task Main(string[] args)
-        //static void Main(string[] args)
         {
-            //Skriv tallene fra 1 til 9, så du får et ni-cifret tal, men hvor 1 skal gå op i det første ciffer, hvor 2 skal gå op i tallet, 
-            //der består af de to første cifre, hvor tallet 3 går op i tallet, der består af de tre første cifre osv.
+            //Skriv tallene fra 1 til 9, så du får et ni-cifret tal, men hvor 1 skal gå op i det første ciffer (læst fra venstre), hvor 2 skal gå op i tallet, 
+            //der består af de to første cifre (læst fra venstre), hvor tallet 3 går op i tallet, der består af de tre første cifre osv.
             //
             // A B C D E F G H I
             ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -21,14 +21,20 @@ namespace FilippaTalOpgave
             // UK : Below is an english translation of the above in danish:
             //
             // UK : MFRs daughters Math exercise
-            // UK : Write all the number from 1 to 9, in such a way that you get a 9 digit number, where 1 can be divided into the first digit, where 2 can be divided into the first 2 digits,
+            // UK : Write all the number from 1 to 9, in such a way that you get a 9 digit number, where 1 can be divided into the first digit(from left side), where 2 can be divided into the first 2 digits(from left side),
             // UK : where 3 can be divided into the first 3 digits, where 4 can be divided into the first 4 digits ...
+
             int minimum = 123456789;
             int maximum = 987654321;
 
             Stopwatch s = new Stopwatch();
-            //JBD COMMENTED OUT ON 11-12-2022 : s.Start();
+            
+            int workerThreads=0;
+            int completionPortThreads =0;
+            ThreadPool.GetMaxThreads(out workerThreads, out completionPortThreads);
 
+            Console.WriteLine();
+            Console.WriteLine("workerThreads : " + workerThreads + "          completionPortThreads : " + completionPortThreads);
             Console.WriteLine();
             Console.Write("Type 1 for non multi threaded or type 4 for multithreaded  THIS MULTITHREADING NEEDS TO BE FIXED :  ");
             ///TODO : 13-09-2020 : Fix that the threads keeps running although one thread has found the result. https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/how-to-cancel-a-task-and-its-children
@@ -47,16 +53,13 @@ namespace FilippaTalOpgave
                     s.Start();
                     await FindTheResultForTheExerciseUsingThreading(minimum, maximum);
                 }
+                
                 if (choice != 1 && choice != 4)
                 {
                     Console.WriteLine("PROBLEMS TO BE HANDLED, not a valid option entered");
                 }
             };
             
-            //FindTheResultForTheExercise(minimum, maximum);
-
-            //await FindTheResultForTheExerciseUsingThreading(minimum, maximum);   //JAN BONDE 09-05-2020 : Use this in case the threading is to run. But it's not faster than : FindTheResultForTheExercise(minimum, maximum). At least not at JBD personal PC.;
-
             s.Stop();
             Console.WriteLine($"Time used (HH:MM:SS:...) = {s.Elapsed}");
             Console.ReadLine();
@@ -67,14 +70,27 @@ namespace FilippaTalOpgave
         /// <param name="minimum"></param>
         /// <param name="maximum"></param>
         /// <returns></returns>
-        static async Task FindTheResultForTheExerciseUsingThreading(int minimum, int maximum)
+        //static async Task FindTheResultForTheExerciseUsingThreading(int minimum, int maximum)
+        public static async Task FindTheResultForTheExerciseUsingThreading(int minimum, int maximum)
         {
-            Task t1 = Task.Run(() => FindTheResultForTheExercise(minimum, 300000000));
-            Task t2 = Task.Run(() => FindTheResultForTheExercise(300000001, 500000000));
-            Task t3 = Task.Run(() => FindTheResultForTheExercise(500000001, 700000000));
-            Task t4 = Task.Run(() => FindTheResultForTheExercise(700000001, maximum));
-            //await Task.WhenAll(t1, t2, t3, t4);
-            await Task.WhenAny(t1, t2, t3, t4);
+            var token = cts.Token;
+            Task t1 = Task.Run(() => FindTheResultForTheExerciseCancellationToken(minimum, 300000000, token));
+            Task t2 = Task.Run(() => FindTheResultForTheExerciseCancellationToken(300000001, 500000000, token));
+            Task t3 = Task.Run(() => FindTheResultForTheExerciseCancellationToken(500000001, 700000000, token));
+            Task t4 = Task.Run(() => FindTheResultForTheExerciseCancellationToken(700000001, maximum, token));
+
+            try
+            {
+                await Task.WhenAny(t1, t2, t3, t4);
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine("CATCH");
+            }
+            finally
+            {
+                cts.Dispose();
+            }
         }
 
         /// <summary>
@@ -90,7 +106,11 @@ namespace FilippaTalOpgave
             for (int i = inputMinimum; i <= inputMaximum; i++)
             {
                 if (i % 10000000 == 0)
-                   Console.WriteLine($"Reached this far : {i}");
+                {
+                    //JBD 27-12-2022 COMMENTED OUT : Console.WriteLine($"Reached this far : {i}");
+                    Console.WriteLine($"Reached this far : {i}   using threadId {Thread.CurrentThread.ManagedThreadId}");
+                }
+
                 if (i % 2 > 0) // tallet skal være ulige (da hvert andet ciffer er lige må de andre være ulige og altså overordnet set et ulige tal).
                                // UK: the result is an odd number (as every second digit is even then the other digits are odd).    
                     if (IsEveryDigitUniqAndEveryAlternateDigitEvenAndOdd(i))
@@ -115,6 +135,60 @@ namespace FilippaTalOpgave
             if (temp == 0)
                 Console.WriteLine($"from {inputMinimum} to {inputMaximum}       Result is not on the Thread that handled this interval - Resultatet er ikke på tråden som håndterede dette interval");
         }
+
+        /// <summary>
+        /// UK : This is the method that finds the result for the exercise (using CancellationToken).
+        /// </summary>
+        /// <param name="inputMinimum"></param>
+        /// <param name="inputMaximum"></param>
+        //private static void FindTheResultForTheExercise(int inputMinimum, int inputMaximum)
+        //static async Task FindTheResultForTheExercise(int inputMinimum, int inputMaximum,)
+        public static void FindTheResultForTheExerciseCancellationToken(int inputMinimum, int inputMaximum, CancellationToken token)
+        {
+            int temp = 0;
+            for (int i = inputMinimum; i <= inputMaximum; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    //Console.WriteLine($"from {inputMinimum} to {inputMaximum}       Resultet er  -  Correct answer is : {temp} ");
+                    //Console.WriteLine("");
+                    
+                    break;
+                    //token.ThrowIfCancellationRequested();
+                    //Console.ReadLine();
+                }  
+                                
+                if (i % 10000000 == 0)
+                {
+                    Console.WriteLine($"Reached this far : {i}   using threadId {Thread.CurrentThread.ManagedThreadId}");
+                }
+
+                if (i % 2 > 0) // tallet skal være ulige (da hvert andet ciffer er lige må de andre være ulige og altså overordnet set et ulige tal).
+                               // UK: the result is an odd number (as every second digit is even then the other digits are odd).    
+                    if (IsEveryDigitUniqAndEveryAlternateDigitEvenAndOdd(i))
+                        if (DoesXDivideIntoTheNumber(i, 9))
+                            if (DoesXDivideIntoTheNumber(i, 8))
+                                if (DoesXDivideIntoTheNumber(i, 7))
+                                    if (DoesXDivideIntoTheNumber(i, 6))
+                                        if (DoesXDivideIntoTheNumber(i, 5))
+                                            if (DoesXDivideIntoTheNumber(i, 4))
+                                                if (DoesXDivideIntoTheNumber(i, 3))
+                                                    if (DoesXDivideIntoTheNumber(i, 2))
+                                                        if (DoesXDivideIntoTheNumber(i, 1))
+                                                        {
+                                                            temp = i;
+                                                            cts.Cancel();
+                                                            //HERHERHERHER break;
+                                                        }
+            }
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            if (temp > 0)
+            Console.WriteLine($" from {inputMinimum} to {inputMaximum}       Resultet er  -  Correct answer is : {temp}");
+        }
+
+
 
         /// <summary>
         /// Finder ud af om X går på i tallet.
@@ -169,7 +243,7 @@ namespace FilippaTalOpgave
 
             int[] array = { int.Parse(A), int.Parse(B), int.Parse(C), int.Parse(D), int.Parse(E), int.Parse(F), int.Parse(G), int.Parse(H), int.Parse(I) };
 
-            for (int i = 0; i < array.Length; i++) //tallet "0" er ikke OK. UK: Digit "0" is not OK as it is not one of the nine digits.
+            for (int i = 0; i < array.Length; i++) //tallet "0" er ikke OK. UK: Digit "0" is not OK as it is not one of the nine digits to be used.
             {
                 if (array[i] == 0)
                     return false;
